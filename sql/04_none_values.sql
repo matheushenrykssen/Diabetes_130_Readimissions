5acaf9e71dfad076142febae3249ba6000585d34
Henrykssen <matheushenrykssen@gmail.com>
2026-07-22 09:33:46 -0300

    Organization

---
+++ sql/04_none_values.sql
@@ -0,0 +1,30 @@
+                                                
+                                                -- None Values --
+
+INSERT INTO dim_admission_type (
+    admission_type_id,
+    admission_type
+)
+VALUES (
+    6,
+    'None'
+)
+
+INSERT INTO dim_discharge_disposition(
+    discharge_disposition_id,
+    discharge_descripition
+)
+VALUES (
+    18,
+    'None'
+)
+
+
+INSERT INTO dim_admission_source(
+    admission_source_id,
+    admission_source_description
+)
+VALUES (
+    17,
+    'None'
+)
